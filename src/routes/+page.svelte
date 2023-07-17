56948a1db61961caab584a90d24da53cc570b252
sutan mufti <sutan@ambersideenergy.com>
2023-07-17 12:11:21 +0100

    wms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,26 +17,47 @@
             const L = await import('leaflet')
             let map = L.map('map').setView([51.505, -0.09], 13);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 11,
+                maxZoom: 20,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+
+            const wmsLayer = L.tileLayer.wms('https://10.0.0.6/geoserver/wms', {
+                format: 'image/png',
+                transparent: true,
+                layers: "cite:power_lines_fme"
             }).addTo(map);
 
             const layergroup = L.layerGroup()
 
             let bound: LatLngBounds | null = null
+            let centr: LatLng = L.latLng(51.509865, -0.118092);
+            let boundn = 0
             addresses?.forEach(d=>{
                 const m = L.marker([d.position.lat, d.position.lng])
                 if (!bound){
                     const latlng = m.getLatLng()
+                    centr = latlng
                     bound = L.latLngBounds([latlng]) 
+                    boundn+=1
                 } else {
                     bound.extend(m.getLatLng())
+                    boundn+=1
                 }
                 layergroup.addLayer(m)
             })
+            console.log(boundn)
+            console.log('ctr:',centr)
             layergroup.addTo(map)
-            if (bound){
-                map.flyToBounds(bound)
+            if (bound && (boundn>1)){
+                boundn = 0
+                console.log('mapbound:', bound)
+                map.fitBounds(bound)
+            } else if (centr) {
+                console.log('panning to', centr)
+                map.flyTo(centr)
+                // centr = L.latLng(51.509865, -0.118092);
+            } else {
+                map.flyTo(centr)
             }
         }
     })
@@ -78,7 +99,7 @@
         height: 100%;
     }
     .main {
-        height: 50vh;
+        height: 70vh;
         /* width: 50vw; */
         display: block;
         /* position: fixed; */
